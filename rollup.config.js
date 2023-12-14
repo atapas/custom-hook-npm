e2eb5c8d7b26b288e885cba2a7f4c9c83cf123a2
Tapas Adhikary <tapas.adhikary@gmail.com>
2023-12-14 14:22:13 +0530

    (feat) Initial configurations - Added rollup config - Tested with a JS file

---
+++ rollup.config.js
@@ -0,0 +1,9 @@
+export default [
+  {
+    input: 'src/index.js',
+    output: {
+      file: 'dist/index.js',
+      format: 'cjs'
+    }
+  }
+];
---
+++ src/calc.js
@@ -0,0 +1,12 @@
+const sum = (a, b) => {
+  return a + b;
+}
+
+const mul = (a, b) => {
+  return a * b;
+}
+
+export {
+  sum,
+  mul
+}
